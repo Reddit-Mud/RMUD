e7f67fa276508f28a5a840e453dfff2d7b580903
Blecki <jm@omnisu.com>
2014-09-16 18:31:25 -0400

    Implement 'me' keyword in object matcher.

--- RMUD/Parser/Matchers/ObjectMatcher.cs
+++ RMUD/Parser/Matchers/ObjectMatcher.cs
@@ -5,18 +5,38 @@ using System.Text;
 
 namespace RMUD
 {
-    internal class ObjectMatcher : ICommandTokenMatcher
+	[Flags]
+	public enum ObjectMatcherSettings
+	{
+		None = 0,
+		UnderstandMe = 1
+	}
+
+    public class ObjectMatcher : ICommandTokenMatcher
     {
 		public String CaptureName;
+		public ObjectMatcherSettings Settings;
 
-		public ObjectMatcher(String CaptureName)
+		public ObjectMatcher(String CaptureName, ObjectMatcherSettings Settings = ObjectMatcherSettings.UnderstandMe)
 		{
 			this.CaptureName = CaptureName;
+			this.Settings = Settings;
 		}
 
 		public List<PossibleMatch> Match(PossibleMatch State, CommandParser.MatchContext Context)
 		{
 			var R = new List<PossibleMatch>();
+
+			if ((Settings & ObjectMatcherSettings.UnderstandMe) == ObjectMatcherSettings.UnderstandMe)
+			{
+				if (State.Next.Value.ToUpper() == "ME")
+				{
+					var possibleMatch = new PossibleMatch(State.Arguments, State.Next.Next);
+					possibleMatch.Arguments.Upsert(CaptureName, Context.ExecutingActor);
+					R.Add(possibleMatch);
+				}
+			}
+
 			foreach (var thing in Context.ObjectsInScope)
 			{
 				var possibleMatch = new PossibleMatch(State.Arguments, State.Next);
