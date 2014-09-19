903faeb1ed6b23d328e88b3442806f77dc89742c
Blecki <jm@omnisu.com>
2014-09-19 08:31:47 -0400

    Added item to demo area to test inventory command.

--- RMUD/database/static/blecki-demo/cabin.cs
+++ RMUD/database/static/blecki-demo/cabin.cs
@@ -5,5 +5,7 @@
 		Short = "Fishing Boat Cabin";
 
 		OpenLink(RMUD.Direction.EAST, "blecki-demo/deck");
+
+		RMUD.Thing.Move(RMUD.Mud.GetObject("blecki-demo/lighthouse_key") as RMUD.Thing, this);
 	}
 }
---
+++ RMUD/database/static/blecki-demo/lighthouse_key.cs
@@ -0,0 +1,9 @@
+﻿public class lighthouse_key : RMUD.Thing
+{
+	public override void Initialize()
+	{
+		Short = "lighthouse key";
+		Long = "This is a small, old fashioned spade-and-barrel key.";
+		Nouns.Add("LIGHTHOUSE", "KEY", "LIGHT", "HOUSE");
+	}
+}
