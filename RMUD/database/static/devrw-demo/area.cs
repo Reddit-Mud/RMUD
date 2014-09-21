de8d151d23ff0cbf95c18bd2f50d48d1faa03667
DevRW <devrobw@gmail.com>
2014-09-21 19:48:56 -0400

    Updated .csproj to get rid of conflict error. Pushed rest of devrw-demo.

--- RMUD/database/static/devrw-demo/area.cs
+++ RMUD/database/static/devrw-demo/area.cs
@@ -1,14 +1,15 @@
 ﻿public class area : RMUD.Room
 {
+
 	public override void Initialize()
 	{
 		Short = "DevRW's Demo Map (incomplete)";
 		Long = "A portal stands here, its inviting light shimmering brightly. Whispers echo ominously in the air...\n" +
 			"\"Go in, my child. Go in.";
 
-		// Object descriptions here.
+// Object descriptions here.
 		AddScenery("Marble pillars centered around an altar-esque table, indentations notched into the sides where light seems to" +
-			"stream from endlessly.", "portal");
+				"stream from endlessly.", "portal");
 		AddScenery("Endless amounts of colors swirl into a white, hovering sphere.", "light");
 
 		OpenLink(RMUD.Direction.IN, "devrw-demo/shoreline");
