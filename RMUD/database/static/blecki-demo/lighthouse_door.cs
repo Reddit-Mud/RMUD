1fee7fcc1a9cd39c9b9ccaf4964e7b58401c668f
Blecki <jm@omnisu.com>
2014-09-21 02:09:49 -0400

    Added locked door to demo area.

--- RMUD/database/static/blecki-demo/lighthouse_door.cs
+++ RMUD/database/static/blecki-demo/lighthouse_door.cs
@@ -1,11 +1,13 @@
-﻿public class lighthouse_door : RMUD.BasicDoor
+﻿public class lighthouse_door : RMUD.LockedDoor
 {
 	public override void Initialize()
 	{
 		Short = "round steel door";
 		Long = "There is a round steel door in the side of the lighthouse.";
 		Nouns.Add("STEEL", "DOOR", "ROUND");
+		Key = "blecki-demo/lighthouse_key";
 
-		Open = true;
+		Open = false;
+		Locked = true;
 	}
 }
