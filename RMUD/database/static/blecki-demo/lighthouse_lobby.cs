c0c713340c16031d4af7e4cf4c2bc33577ca54c6
Blecki <jm@omnisu.com>
2014-09-22 22:54:10 -0400

    Update descriptions in blecki-demo

--- RMUD/database/static/blecki-demo/lighthouse_lobby.cs
+++ RMUD/database/static/blecki-demo/lighthouse_lobby.cs
@@ -3,6 +3,7 @@
 	public override void Initialize()
 	{
 		Short = "Lighthouse Lobby";
+        Long = "A ponderous emptyness hangs about the bottom of this lighthouse. The walls are plain gray cinderblock, the floor is a slab of greenish concrete, and quite literally nothing surrounds the base of the spiralling staircase that stabs through the ceiling. A few thin windows full of thick glass don't let in enough light.";
 
 		OpenLink(RMUD.Direction.WEST, "blecki-demo/shoreline", RMUD.Mud.GetObject("blecki-demo/lighthouse_door"));
 		OpenLink(RMUD.Direction.UP, "blecki-demo/lighthouse_stairway");
--- RMUD/database/static/blecki-demo/lighthouse_stairway.cs
+++ RMUD/database/static/blecki-demo/lighthouse_stairway.cs
@@ -3,6 +3,7 @@
 	public override void Initialize()
 	{
 		Short = "Lighthouse Stairway";
+        Long = "A series of cast iron treads climb through the center of the lighthouse. The soles of countless boots and the souls of countless passers have buffed them to just the faintest of a gleam. Dust stands still in the air, illuminated by beams piercing the dull trapped eternity inside. The windows are always placed too high to see out, no matter how many stairs you climb.";
 
 		OpenLink(RMUD.Direction.WEST, "blecki-demo/lighthouse_balcony");
 		OpenLink(RMUD.Direction.DOWN, "blecki-demo/lighthouse_lobby");
