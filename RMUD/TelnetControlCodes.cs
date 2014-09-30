c6ee0b637a112ee06a67894c4beeb3a5ec6b1b36
Blecki <jm@omnisu.com>
2014-09-30 08:52:38 -0400

    Beginings of MCCP support

---
+++ RMUD/TelnetControlCodes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD
+{
+    public enum TelnetControlCodes
+    {
+        NULL = 0,
+        LineFeed = 10,
+        CarriageReturn = 13,
+
+        Bell = 7,
+        BackSpace = 8,
+        HorizontalTab = 9,
+        VerticalTab = 11,
+        FormFeed = 12,
+
+        MCCPCompress = 86,
+
+        EndSubParameters = 240,
+        Nop = 241,
+        DataMark = 242,
+        Break = 243,
+        SuspendInterrupt = 244,
+        AbortOutput = 245,
+        AreYouThere = 246,
+        EraseCharacter = 247,
+        EraseLine = 248,
+        GoAhead = 249,
+        SubParameters = 250,
+
+        Will = 251,
+        Wont = 252,
+        Do = 253,
+        Dont = 254,
+        IAC = 255
+    }
+}
