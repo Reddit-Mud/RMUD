1010069a8916189b70785d5ff8ef0d3131a0054d
Blecki <jm@omnisu.com>
2014-10-11 09:35:34 -0400

    fix #83

--- RMUD/Program.cs
+++ RMUD/Program.cs
@@ -22,79 +22,86 @@ namespace RMUD
 			}
 		}
 
-		static void Main(string[] args)
-		{
-			var commandLineOptions = new CommandLineOptions();
-			var error = CommandLine.ParseCommandLine(commandLineOptions);
+        static void Main(string[] args)
+        {
+            var commandLineOptions = new CommandLineOptions();
+            var error = CommandLine.ParseCommandLine(commandLineOptions);
 
-			if (error != CommandLine.Error.Success)
-			{
-				Console.WriteLine("Command line error: " + error);
-				return;
-			}
+            if (error != CommandLine.Error.Success)
+            {
+                Console.WriteLine("Command line error: " + error);
+                return;
+            }
 
-			TelnetClientSource telnetListener = null;
+            TelnetClientSource telnetListener = null;
 
-			if (Mud.Start(commandLineOptions.DATABASEPATH))
-			{
-				telnetListener = new TelnetClientSource();
-				telnetListener.Port = commandLineOptions.PORT;
-				telnetListener.Listen();
-			}
+            if (Mud.Start(commandLineOptions.DATABASEPATH))
+            {
+                telnetListener = new TelnetClientSource();
+                telnetListener.Port = commandLineOptions.PORT;
+                telnetListener.Listen();
+            }
 
-			while (true)
-			{
-				var command = Console.ReadLine();
-
-				if (command.ToUpper() == "STOP")
-					break;
-				else if (command.ToUpper() == "CLIENTS")
-				{
-					Mud.DatabaseLock.WaitOne();
-
-					foreach (var client in Mud.ConnectedClients)
-					{
-						Console.Write(client.ConnectionDescription);
-						if (client.Player != null)
-						{
-							Console.Write(" -- ");
-							Console.Write(client.Player.Short);
-						}
-						Console.WriteLine();
-					}
-
-					Mud.DatabaseLock.ReleaseMutex();
-				}
-				else if (command.ToUpper() == "MEMORY")
-				{
-					var mem = System.GC.GetTotalMemory(false);
-					var kb = mem / 1024.0f;
-					Console.WriteLine("Memory usage: " + String.Format("{0:n0}", kb) + " kb");
-					Console.WriteLine("Named objects loaded: " + Mud.NamedObjects.Count);
-				}
-                else if (command.ToUpper() == "HEARTBEAT")
+            try
+            {
+                while (true)
                 {
-                    Mud.DatabaseLock.WaitOne();
-                    Console.WriteLine("Heartbeat interval: {0} Objects: {1} HID: {2}",
-                        Mud.SettingsObject.HeartbeatInterval, 
-                        Mud.ObjectsRegisteredForHeartbeat.Count, 
-                        Mud.HeartbeatID);
-                    foreach (var Object in Mud.ObjectsRegisteredForHeartbeat)
-                        Console.WriteLine(Object.ToString());
-                    Mud.DatabaseLock.ReleaseMutex();
-                }
-                else if (command.ToUpper() == "TIME")
-                {
-                    Mud.DatabaseLock.WaitOne();
-                    Console.WriteLine("Current time in game: {0}", Mud.TimeOfDay);
-                    Console.WriteLine("Advance rate: {0} per heartbeat",
-                        Mud.SettingsObject.ClockAdvanceRate);
-                    Mud.DatabaseLock.ReleaseMutex();
-                }
-			}
+                    var command = Console.ReadLine();
 
-			telnetListener.Shutdown();
-			Mud.Shutdown();
-		}
+                    if (command.ToUpper() == "STOP")
+                        break;
+                    else if (command.ToUpper() == "CLIENTS")
+                    {
+                        Mud.DatabaseLock.WaitOne();
+
+                        foreach (var client in Mud.ConnectedClients)
+                        {
+                            Console.Write(client.ConnectionDescription);
+                            if (client.Player != null)
+                            {
+                                Console.Write(" -- ");
+                                Console.Write(client.Player.Short);
+                            }
+                            Console.WriteLine();
+                        }
+
+                        Mud.DatabaseLock.ReleaseMutex();
+                    }
+                    else if (command.ToUpper() == "MEMORY")
+                    {
+                        var mem = System.GC.GetTotalMemory(false);
+                        var kb = mem / 1024.0f;
+                        Console.WriteLine("Memory usage: " + String.Format("{0:n0}", kb) + " kb");
+                        Console.WriteLine("Named objects loaded: " + Mud.NamedObjects.Count);
+                    }
+                    else if (command.ToUpper() == "HEARTBEAT")
+                    {
+                        Mud.DatabaseLock.WaitOne();
+                        Console.WriteLine("Heartbeat interval: {0} Objects: {1} HID: {2}",
+                            Mud.SettingsObject.HeartbeatInterval,
+                            Mud.ObjectsRegisteredForHeartbeat.Count,
+                            Mud.HeartbeatID);
+                        foreach (var Object in Mud.ObjectsRegisteredForHeartbeat)
+                            Console.WriteLine(Object.ToString());
+                        Mud.DatabaseLock.ReleaseMutex();
+                    }
+                    else if (command.ToUpper() == "TIME")
+                    {
+                        Mud.DatabaseLock.WaitOne();
+                        Console.WriteLine("Current time in game: {0}", Mud.TimeOfDay);
+                        Console.WriteLine("Advance rate: {0} per heartbeat",
+                            Mud.SettingsObject.ClockAdvanceRate);
+                        Mud.DatabaseLock.ReleaseMutex();
+                    }
+                }
+            }
+            catch (Exception e) //Who knows what Mono might throw?
+            {
+                while (true) { } //We still don't want to exit.
+            }
+
+            telnetListener.Shutdown();
+            Mud.Shutdown();
+        }
     }
 }
