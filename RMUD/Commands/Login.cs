eb10563568bdb4bdb97c4bc6f8be6bdfc5622bf0
Michael Brewer <constablebrew@gmail.com>
2014-10-17 15:18:56 -0700

    Added the new cs files

---
+++ RMUD/Commands/Login.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD.Commands
+{
+	internal class Login : CommandFactory
+	{
+        public override void Create(CommandParser Parser)
+        {
+            Parser.AddCommand(
+                new Sequence(
+                    new KeyWord("LOGIN", false),
+                    new FailIfNoMatches(
+                        new SingleWord("USERNAME"),
+                        "You must supply a username.\r\n"),
+                    new FailIfNoMatches(
+                        new SingleWord("PASSWORD"),
+                        "You must supply a password.\r\n")),
+                new LoginProcessor(),
+                "Login to an existing account.\r\n");
+        }
+	}
+
+	internal class LoginProcessor : CommandProcessor
+	{
+        public void Perform(PossibleMatch Match, Actor Actor)
+        {
+            if (Actor != null)
+            {
+                Mud.SendMessage(Actor.ConnectedClient, "You are already logged in.\r\n");
+                return;
+            }
+
+            Mud.CommandTimeoutEnabled = false;
+
+            var client = Match.Arguments["CLIENT"] as Client;
+            var userName = Match.Arguments["USERNAME"].ToString();
+            var password = Match.Arguments["PASSWORD"].ToString();
+
+            var existingAccount = Mud.FindAccount(userName);
+            if (existingAccount == null || Mud.VerifyAccount(existingAccount, password) == false)
+            {
+                Mud.SendMessage(client, "Could not verify account.\r\n");
+                return;
+            }
+
+            LoginCommandHandler.LogPlayerIn(client, existingAccount);
+        }        
+	}
+}
---
+++ RMUD/Commands/Register.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD.Commands
+{
+	internal class Register : CommandFactory
+	{
+        public override void Create(CommandParser Parser)
+        {
+            Parser.AddCommand(
+                new Sequence(
+                    new KeyWord("REGISTER", false),
+                    new FailIfNoMatches(
+                        new SingleWord("USERNAME"),
+                        "You must supply a username.\r\n"),
+                    new FailIfNoMatches(
+                        new SingleWord("PASSWORD"),
+                        "You must supply a password.\r\n")),
+                    new RegistrationProcessor(),
+                    "Create a new account.\r\n");
+        }
+	}
+
+	internal class RegistrationProcessor : CommandProcessor
+    {
+        public void Perform(PossibleMatch Match, Actor Actor)
+        {
+            var client = Match.Arguments["CLIENT"] as Client;
+            var userName = Match.Arguments["USERNAME"].ToString();
+            var password = Match.Arguments["PASSWORD"].ToString();
+
+            var existingAccount = Mud.FindAccount(userName);
+            if (existingAccount != null)
+            {
+                Mud.SendMessage(client, "That account already exists.\r\n");
+                return;
+            }
+
+            var newAccount = Mud.CreateAccount(userName, password);
+            LoginCommandHandler.LogPlayerIn(client, newAccount);
+        }
+    }
+}
