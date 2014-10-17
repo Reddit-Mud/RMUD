d8b6493e27c628b717d3ac8cd0eb1af2fa0a54eb
Blecki <jm@omnisu.com>
2014-10-16 22:58:38 -0400

    Availability of the console commands is an option in settings. Turn them off to run the server as a deamon.

--- RMUD/Program.cs
+++ RMUD/Program.cs
@@ -42,86 +42,96 @@ namespace RMUD
                 telnetListener.Listen();
             }
 
-            try
+            if (Mud.SettingsObject.UseConsoleCommands)
             {
-                while (true)
+                try
                 {
-                    var command = Console.ReadLine();
-
-                    if (command.ToUpper() == "STOP")
-                        break;
-                    else if (command.ToUpper() == "CLIENTS")
+                    while (true)
                     {
-                        Mud.DatabaseLock.WaitOne();
+                        var command = Console.ReadLine();
 
-                        foreach (var client in Mud.ConnectedClients)
+                        if (command.ToUpper() == "STOP")
+                            break;
+                        else if (command.ToUpper() == "CLIENTS")
                         {
-                            Console.Write(client.ConnectionDescription);
-                            if (client.Player != null)
-                            {
-                                Console.Write(" -- ");
-                                Console.Write(client.Player.Short);
-                            }
-                            Console.WriteLine();
-                        }
+                            Mud.DatabaseLock.WaitOne();
 
-                        Mud.DatabaseLock.ReleaseMutex();
-                    }
-                    else if (command.ToUpper() == "MEMORY")
-                    {
-                        var mem = System.GC.GetTotalMemory(false);
-                        var kb = mem / 1024.0f;
-                        Console.WriteLine("Memory usage: " + String.Format("{0:n0}", kb) + " kb");
-                        Console.WriteLine("Named objects loaded: " + Mud.NamedObjects.Count);
-                    }
-                    else if (command.ToUpper() == "HEARTBEAT")
-                    {
-                        Mud.DatabaseLock.WaitOne();
-                        Console.WriteLine("Heartbeat interval: {0} Objects: {1} HID: {2}",
-                            Mud.SettingsObject.HeartbeatInterval,
-                            Mud.ObjectsRegisteredForHeartbeat.Count,
-                            Mud.HeartbeatID);
-                        foreach (var Object in Mud.ObjectsRegisteredForHeartbeat)
-                            Console.WriteLine(Object.ToString());
-                        Mud.DatabaseLock.ReleaseMutex();
-                    }
-                    else if (command.ToUpper() == "TIME")
-                    {
-                        Mud.DatabaseLock.WaitOne();
-                        Console.WriteLine("Current time in game: {0}", Mud.TimeOfDay);
-                        Console.WriteLine("Advance rate: {0} per heartbeat",
-                            Mud.SettingsObject.ClockAdvanceRate);
-                        Mud.DatabaseLock.ReleaseMutex();
-                    }
-                    else if (command.ToUpper() == "SAVE")
-                    {
-                        Mud.DatabaseLock.WaitOne();
-                        Console.WriteLine("Saving named objects to file...");
-                        foreach(var data in Mud.NamedObjects){
-                            Console.WriteLine(data.Key);
-                            var dto = new DTO();
-                            var properties = (from x in data.Value.GetType().GetProperties() select x);
-                            dto.Data = properties.ToDictionary
-                            (
-                                x => x.Name, 
-                                x => (x.GetGetMethod().Invoke(data.Value, null) ?? "").ToString()
-                            );
-                            Mud.SaveDTO(data.Key, dto);
+                            foreach (var client in Mud.ConnectedClients)
+                            {
+                                Console.Write(client.ConnectionDescription);
+                                if (client.Player != null)
+                                {
+                                    Console.Write(" -- ");
+                                    Console.Write(client.Player.Short);
+                                }
+                                Console.WriteLine();
+                            }
+
+                            Mud.DatabaseLock.ReleaseMutex();
                         }
-                        Console.WriteLine("Saved all named objects.");
+                        else if (command.ToUpper() == "MEMORY")
+                        {
+                            var mem = System.GC.GetTotalMemory(false);
+                            var kb = mem / 1024.0f;
+                            Console.WriteLine("Memory usage: " + String.Format("{0:n0}", kb) + " kb");
+                            Console.WriteLine("Named objects loaded: " + Mud.NamedObjects.Count);
+                        }
+                        else if (command.ToUpper() == "HEARTBEAT")
+                        {
+                            Mud.DatabaseLock.WaitOne();
+                            Console.WriteLine("Heartbeat interval: {0} Objects: {1} HID: {2}",
+                                Mud.SettingsObject.HeartbeatInterval,
+                                Mud.ObjectsRegisteredForHeartbeat.Count,
+                                Mud.HeartbeatID);
+                            foreach (var Object in Mud.ObjectsRegisteredForHeartbeat)
+                                Console.WriteLine(Object.ToString());
+                            Mud.DatabaseLock.ReleaseMutex();
+                        }
+                        else if (command.ToUpper() == "TIME")
+                        {
+                            Mud.DatabaseLock.WaitOne();
+                            Console.WriteLine("Current time in game: {0}", Mud.TimeOfDay);
+                            Console.WriteLine("Advance rate: {0} per heartbeat",
+                                Mud.SettingsObject.ClockAdvanceRate);
+                            Mud.DatabaseLock.ReleaseMutex();
+                        }
+                        else if (command.ToUpper() == "SAVE")
+                        {
+                            Mud.DatabaseLock.WaitOne();
+                            Console.WriteLine("Saving named objects to file...");
+                            foreach (var data in Mud.NamedObjects)
+                            {
+                                Console.WriteLine(data.Key);
+
+                                try
+                                {
+                                    var dto = new DTO();
+                                    var properties = (from x in data.Value.GetType().GetProperties() select x);
+                                    dto.Data = properties.ToDictionary
+                                    (
+                                        x => x.Name,
+                                        x => (x.GetGetMethod().Invoke(data.Value, null) ?? "").ToString()
+                                    );
+                                    Mud.SaveDTO(data.Key, dto);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                }
+                            }
+                            Console.WriteLine("Saved all named objects.");
+                        }
                     }
                 }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR:", e.Message, e.Source, e.StackTrace, e.Data);
+                }
+
             }
-            catch (Exception e) //Who knows what Mono might throw?
-            {
-                Console.WriteLine("ERROR:", e.Message, e.Source, e.StackTrace, e.Data);
-                //Infinite loop??? while (true) { } //We still don't want to exit.
-            }
-            finally
-            {
-                telnetListener.Shutdown();
-                Mud.Shutdown();
-            }
+
+            telnetListener.Shutdown();
+            Mud.Shutdown();
         }
     }
 }
