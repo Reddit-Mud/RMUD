904382d63f045b8e594daaf60ef16b38a22f0b9a
Blecki <jm@omnisu.com>
2014-10-21 08:36:47 -0400

    Save command saves the database

---
+++ RMUD/Commands/Purge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD.Commands
+{
+	internal class Purge : CommandFactory
+	{
+        public override void Create(CommandParser Parser)
+        {
+            Parser.AddCommand(
+                new Sequence(
+                    new RankGate(500),
+                    new KeyWord("PURGE", false)),
+                new PurgeProcessor(),
+                "Purge the dynamic database from disc.");
+        }
+	}
+
+	internal class PurgeProcessor : CommandProcessor
+	{
+		public void Perform(PossibleMatch Match, Actor Actor)
+		{
+
+		}
+	}
+
+}
---
+++ RMUD/Commands/Save.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD.Commands
+{
+	internal class Save : CommandFactory
+	{
+        public override void Create(CommandParser Parser)
+        {
+            Parser.AddCommand(
+                new Sequence(
+                    new RankGate(500),
+                    new KeyWord("SAVE", false)),
+                new PurgeProcessor(),
+                "Save game state to disc.");
+        }
+	}
+
+	internal class SaveProcessor : CommandProcessor
+	{
+		public void Perform(PossibleMatch Match, Actor Actor)
+		{
+            Mud.CommandTimeoutEnabled = false;
+
+            Mud.SendGlobalMessage("The database is being saved. There may be a brief delay.\r\n");
+            Mud.SendPendingMessages();
+
+            var saved = Mud.SaveActiveInstances();
+
+            Mud.SendGlobalMessage("The database has been saved.\r\n");
+            Mud.SendMessage(Actor, String.Format("I saved {0} persistent objects.\r\n", saved));
+		}
+	}
+
+}
