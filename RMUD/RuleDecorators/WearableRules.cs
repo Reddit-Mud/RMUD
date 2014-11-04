6ff5599e9dd221ba479810df0457d440e8bfc90c
Blecki <jm@omnisu.com>
2014-11-03 23:13:37 -0500

    WearableRules

---
+++ RMUD/RuleDecorators/WearableRules.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD
+{
+	public interface WearableRules
+	{
+		CheckRule CheckWear(Actor Actor);
+		CheckRule CheckRemove(Actor Actor);
+		RuleHandlerFollowUp HandleWear(Actor Actor);
+		RuleHandlerFollowUp HandleRemove(Actor Actor);
+	}
+}
