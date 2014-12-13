c170e0f8a301ec28f088b2b5a4cce537ed9bf7a6
Blecki <jm@omnisu.com>
2014-12-13 10:08:12 -0500

    Generate mudobject's add rule methods too.

---
+++ RMUD/Rules/MudObjectAddRuleGen.cs
@@ -0,0 +1,58 @@
+﻿//This is generated code. Do not modify this file; modify the template that produces it.
+
+using System;
+
+namespace RMUD
+{
+	public partial class MudObject
+	{	
+		public RuleBuilder<T0, RuleResult> AddActionRule<T0>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, RuleResult>(Name);
+		}
+
+		public RuleBuilder<T0, RT> AddValueRule<T0, RT>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, RT>(Name);
+		}
+
+		public RuleBuilder<T0, T1, RuleResult> AddActionRule<T0, T1>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, RuleResult>(Name);
+		}
+
+		public RuleBuilder<T0, T1, RT> AddValueRule<T0, T1, RT>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, RT>(Name);
+		}
+
+		public RuleBuilder<T0, T1, T2, RuleResult> AddActionRule<T0, T1, T2>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, T2, RuleResult>(Name);
+		}
+
+		public RuleBuilder<T0, T1, T2, RT> AddValueRule<T0, T1, T2, RT>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, T2, RT>(Name);
+		}
+
+		public RuleBuilder<T0, T1, T2, T3, RuleResult> AddActionRule<T0, T1, T2, T3>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, T2, T3, RuleResult>(Name);
+		}
+
+		public RuleBuilder<T0, T1, T2, T3, RT> AddValueRule<T0, T1, T2, T3, RT>(String Name)
+		{
+			if (Rules == null) Rules = new RuleSet();
+			return Rules.AddRule<T0, T1, T2, T3, RT>(Name);
+		}
+
+	}
+}
