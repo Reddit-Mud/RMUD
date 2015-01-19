f603d6968a38ef789bc2992c19e224aabe8d9ec0
Blecki <jm@omnisu.com>
2015-01-19 13:43:01 -0500

    Add command to dump rule defs so they can be incorporated in transform tool, use tool with akkoteaque example.

---
+++ RMUD/Modules/Admin/DumpRuleDefs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RMUD.Modules.Admin
+{
+    internal class DumpRuleDefs : CommandFactory
+    {
+        public override void Create(CommandParser Parser)
+        {
+            Parser.AddCommand(
+                Sequence(
+                    RequiredRank(500),
+                    KeyWord("!DUMPRULES")))
+                .Manual("Dumps all defined global rule books to a file.")
+                .ProceduralRule((match, actor) =>
+                {
+                    MudObjectTransformTool.RulePattern.DumpStandardRuleArgumentTypes("ruledefs.txt");
+                    return PerformResult.Continue;
+                });
+        }
+
+        
+    }
+}
--- RMUD/Modules/StandardActions/Close.cs
+++ RMUD/Modules/StandardActions/Close.cs
@@ -28,7 +28,7 @@ namespace RMUD.Modules.StandardActions
 
         public static void AtStartup(RuleEngine GlobalRules)
         {
-            GlobalRules.DeclareCheckRuleBook<MudObject, MudObject>("can close?", "[Actor, Item] : Determine if the item can be closed.");
+            GlobalRules.DeclareCheckRuleBook<MudObject, MudObject>("can close?", "[Actor, Item] : Determine if the item can be closed.", "actor", "item");
 
             GlobalRules.DeclarePerformRuleBook<MudObject, MudObject>("closed", "[Actor, Item] : Handle the item being closed.");
 
