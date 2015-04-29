c1cf71f9d62530572833298558c4891af0053bac
Blecki <jm@omnisu.com>
2015-04-29 18:11:22 -0400

    Testing VS github fix

--- Core/ModuleInfo.cs
+++ Core/ModuleInfo.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace RMUD
 {
+    /// <summary>
+    /// Define in modules to A) Identify the assembly as a module and B) Tell the engine what namespace
+    /// to load from.
+    /// </summary>
     public class ModuleInfo
     {
         public String BaseNameSpace = "Don't load me.";
